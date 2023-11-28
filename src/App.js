063a294d37a1974aeb8edba34b4f28521eba6f2a
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-11-27 16:40:25 -0800

    created separate files for each component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import  FirstComponent from './components/learning-examples/FirstComponent';
+import  ThirdComponent from './components/learning-examples/ThirdComponent'
+import  SecondComponent from './components/learning-examples/SecondComponent'
+import  FourthComponent from './components/learning-examples/FourthComponent'
+
 import { Component } from 'react';
 
 function App() {
@@ -12,29 +17,6 @@ function App() {
       <FourthComponent />
     </div>
   );
-}
-function FirstComponent(){
-  return (
-    <div className='FirstComponent'>FirstComponent</div>
-  );
-}
-
-function SecondComponent(){
-  return (
-    <div className='SecondComponent'>SecondComponent</div>
-  );
-}
-class ThirdComponent extends Component{
-  render(){
-      return (
-        <div className='ThirdComponent'>ThirdComponent</div>
-      );
-    }
-}
-class FourthComponent extends Component{
-  render(){
-    return (<div className='FourthComponent'>FourthComponent</div>);
-  }
 }
 
 export default App;
---
+++ src/components/learning-examples/FourthComponent.jsx
@@ -0,0 +1,8 @@
+ import React,{Component} from "react";
+ 
+ class FourthComponent extends Component{
+  render(){
+    return (<div className='FourthComponent'>FourthComponent</div>);
+  }
+}
+export default FourthComponent;
