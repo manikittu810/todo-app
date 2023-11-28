aea346723f89ad002b1bdea6d4e867c01e18d856
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-11-28 15:11:01 -0800

    Adding state to counter

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,13 +1,22 @@
 import './Counter.css'
+import { useState } from 'react';
+
 
 export default function Counter(){
+
+//state is returning two values , it returns first one is a value and second one is a function.
+//[0,f] the state contains an initial value of the state as the first value and  it is passed as a parameter to the state.
+
+    const [count,setCount] = useState(0);
+
     function incrementCounterFunction(){
-        console.log('Increment clicked');
+        setCount(count+1);
+        console.log(count);
     };
 
     return(
     <div className="Counter">
-        <span className="count">0</span>
+        <span className="count">{count}</span>
         <div>
             <button className="counterButton" 
             onClick={incrementCounterFunction}>+1</button>
