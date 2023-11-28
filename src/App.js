5d31255ba6d6373a438672919045c83c227060ae
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-11-28 13:41:21 -0800

    started writing functions for counter App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import LearningComponents from "./components/learning-examples/LearningComponents.jsx";
+import Counter from './components/counter/Counter'
+// import LearningComponents from "./components/learning-examples/LearningComponents.jsx";
 
 
 function App() {
   return (
     <div className="App">
-      <LearningComponents/>
+      <Counter/>
     </div>
   );
 }
