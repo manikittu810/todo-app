4bebfa059b9b9ce96b9602e6e162d029e8b3ac69
manikittu810 <msanganabatla@csu.fullerton.edu>
2023-12-04 17:42:47 -0800

    Refactoring React Counter Component

--- src/components/counter/CounterButton.jsx
+++ src/components/counter/CounterButton.jsx
@@ -5,20 +5,15 @@ export default function CounterButton({by,incrementMethod,decrementMethod}){
 
     //state is returning two values , it returns first one is a value and second one is a function.
     //[0,f] the state contains an initial value of the state as the first value and  it is passed as a parameter to the state.
-            function incrementCounterFunction(){
-            incrementMethod(by);
-        };
-        function decrementCounterFunction(){
-            decrementMethod(by);
-        };
+       
     
         return(
         <div className="Counter">
             <div>
                 <button className="counterButton" 
-                onClick={incrementCounterFunction}>+{by}</button>
+                onClick={() => incrementMethod(by)}>+{by}</button>
                 <button className="counterButton" 
-                onClick={decrementCounterFunction}>-{by}</button>
+                onClick={() => decrementMethod(by)}>-{by}</button>
             </div>
            </div>
         );
