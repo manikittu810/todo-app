c0d04b4fdde58664964a53e23ed123fb04e49e75
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-01-29 16:07:43 -0800

    Setting isAuthenticated into React State - Auth Context

--- src/components/todo/HeaderComponent.jsx
+++ src/components/todo/HeaderComponent.jsx
@@ -6,7 +6,13 @@ function HeaderComponent(){
     // const authContext = useContext(AuthContext);
 
     const authContext = useAuth();
-    console.log(authContext.number);
+    const isAuthenticated = authContext.isAuthenticated
+    // console.log(authContext);
+    // console.log(authContext.number);
+
+    function logout(){
+        authContext.setAuthenticated (false);
+    }
 
     return(
         <header className="border-bottom border-light border-5 mb-5 p-2">
@@ -16,13 +22,25 @@ function HeaderComponent(){
                     <a className="navbar-brand ms-2 fs-2 fw-bold text-black" href="https://www.linkedin.com/in/msanganabatla/">My_Linkedin</a>
                     <div className="collapse navbar-collapse">
                         <ul className="navbar-nav">
-                            <li className="nav-item fs-5"><Link className="nav-link" to="/welcome/SMK">Home</Link></li>
-                            <li className="nav-item fs-5"><Link className="nav-link" to="/todos">Todos</Link></li>
+                            <li className="nav-item fs-5">
+                                {isAuthenticated && <Link className="nav-link" to="/welcome/SMK">Home</Link>}
+                                
+                            </li>
+                            <li className="nav-item fs-5">
+                                {isAuthenticated && <Link className="nav-link" to="/todos">Todos</Link>}
+                                
+                            </li>
                         </ul>
                     </div>
                     <ul className="navbar-nav">
-                        <li className="nav-item fs-5"><Link className="nav-link" to="/login">Login</Link></li>
-                        <li className="nav-item fs-5"><Link className="nav-link" to="/logout">Logout</Link></li>
+                        <li className="nav-item fs-5">
+                            {!isAuthenticated && <Link className="nav-link" to="/login">Login</Link>}
+                            
+                        </li>
+                        <li className="nav-item fs-5">
+                            {isAuthenticated && <Link className="nav-link" to="/logout" onClick={logout}>Logout</Link>
+}
+                        </li>
                     </ul>
                 </nav>
             </div>
--- src/components/todo/LoginComponent.jsx
+++ src/components/todo/LoginComponent.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import {useNavigate} from 'react-router-dom';
+import { useAuth } from './security/AuthContext';
 
 function LoginComponent(){
     const[username,setUsername]=useState('Smk_TodoApp');
@@ -7,6 +8,7 @@ function LoginComponent(){
     const[showSuccessMessage,setShowSuccessMessage] = useState(false);
     const[showErrorMessage,setShowErrorMessage] = useState(false);
     const navigate = useNavigate();
+    const authContext = useAuth();
     
     
     
@@ -19,16 +21,19 @@ function LoginComponent(){
     
     function handleSubmit(){
         if(username==='Smk_TodoApp' && password==='smk123'){
+            authContext.setAuthenticated(true);
             console.log("Success");
             setShowSuccessMessage(true);
             setShowErrorMessage(false);
             navigate(`/welcome/${username}`);
+
         }
         else{
-        console.log("failed");
-        setShowSuccessMessage(false);
-        setShowErrorMessage(true);
-    
+            authContext.setAuthenticated(false);
+            console.log("failed");
+            setShowSuccessMessage(false);
+            setShowErrorMessage(true);
+        
         }
         }
     
