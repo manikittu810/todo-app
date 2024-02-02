89336f1db9f6ea91954cafbb2709f78eaa2327c6
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-02-01 16:33:57 -0800

    removed redundant code

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,50 +1,20 @@
 import {useParams, Link } from "react-router-dom";
 import {useState}  from "react";
-import { retrieveHelloWorldBean,retrieveHelloWorld,retrieveHelloWorldPathVariable } from "./api/HelloWorldApiService";
+import {retrieveHelloWorldPathVariable } from "./api/HelloWorldApiService";
 function WelcomeComponent(){
 
     const {username} = useParams();
 
     const [message,setMessage]= useState(null)
 
-function callHelloWorldRestApi(){
-
-//the below is an asynchronous approach
-
-        console.log('called');
-
-        retrieveHelloWorld()
-        .then((response) => successfulResponse(response))
-        .catch((error) => errorResponse(error))
-        .finally(()=>console.log('cleanup'))
-}
-
-function callHelloWorldBean(response){
-
-    console.log('bean called')
-
-    retrieveHelloWorldBean()
-    .then((response) => successfulResponse1(response))
-    .catch((error) => errorResponse(error))
-    .finally(()=> console.log('cleanUp the bean'))
-}
 
 function callHelloWorldPathVariable(response){
+    //below is an asynchronous approach 
     console.log('path variable called')
     retrieveHelloWorldPathVariable('SMK')
     .then((response) => successfulResponse2(response))
     .catch((error) => errorResponse(error))
     .finally(()=> console.log('cleanUp the bean'))
-}
-
-function successfulResponse(response){
-    console.log(response);
-    setMessage(response.data)
-}
-
-function successfulResponse1(response){
-    console.log(response);
-    setMessage(response.data.message)
 }
 
 function successfulResponse2(response){
@@ -61,12 +31,6 @@ function errorResponse(error){
             <h1>Welcome {username} !</h1>
         <div>
             Your Todos <Link to="/todos">Go here !! </Link>
-        </div>
-        <div>
-            <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>call Hello World</button>
-        </div>
-        <div>
-            <button className="btn btn-success m-5" onClick={callHelloWorldBean}>call Hello World Bean</button>
         </div>
         <div>
             <button className="btn btn-success m-5" onClick={callHelloWorldPathVariable}>call Path variable</button>
--- src/components/todo/api/HelloWorldApiService.js
+++ src/components/todo/api/HelloWorldApiService.js
@@ -6,14 +6,5 @@ import axios from 'axios';
 const apiClient = axios.create(
     {baseURL : 'http://localhost:8080'}
 );
-
-export const  retrieveHelloWorld=()=>apiClient.get('http://localhost:8080/hello-world');
-
-export const retrieveHelloWorldBean = ()=>apiClient.get('http://localhost:8080/hello-world-bean');
-
-// export function retrieveHelloWorld(){
-//     return apiClient.get('http://localhost:8080/hello-world');
-// }
-
 export const  retrieveHelloWorldPathVariable=(username)=>apiClient.get(`http://localhost:8080/hello-world/path-variable/${username}`);
 
