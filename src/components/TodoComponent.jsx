40c496d71042665402b60deb5fecd903e5374670
manikittu810 <msanganabatla@csu.fullerton.edu>
2024-02-04 13:02:57 -0800

    Adding Formik and Moment Libraries to Display Todo React Component

--- src/components/TodoComponent.jsx
+++ src/components/TodoComponent.jsx
@@ -2,10 +2,13 @@ import { useEffect, useState } from 'react';
 import {useParams} from 'react-router-dom';
 import { retrieveTodoApi } from './todo/api/TodoApiService';
 import { useAuth } from './todo/security/AuthContext';
+import { Formik, Field, Form } from 'formik'; 
 
 function TodoComponent(){
 
     const [description,setDescription] = useState('')
+    const [targetDate,setTargetDate] = useState('')
+
 
     const {id} = useParams();
 
@@ -18,6 +21,7 @@ function TodoComponent(){
         retrieveTodoApi(username,id)
         .then(response => {
             setDescription(response.data.description)
+            setTargetDate(response.data.targetDate)
         })
         .catch((error) => console.log('error'))
     }
@@ -26,11 +30,36 @@ function TodoComponent(){
         () => retrieveTodos(),[id]
         )
 
+       function onSubmit(values){
+        console.log(values)
+       } 
+
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                description :{description}
+                <Formik initialValues={{description,targetDate}}
+                enableReinitialize={true}
+                onSubmit={onSubmit}>
+                    {
+                        (props) => (
+                            <Form>
+                                <fieldset className='form-group'>
+                                    <label>Description</label>
+                                    <Field type="text" className="form-control" name="description"/>
+                                </fieldset>
+
+                                <fieldset className='form-group'>
+                                    <label>Target Date</label>
+                                    <Field type="date" className="form-control" name="targetDate"/>
+                                </fieldset>
+                                <div>
+                                    <button className="btn btn-success" type='submit'>Save</button>
+                                </div>
+                            </Form>
+                        )
+                    }
+                </Formik>
             </div>
         </div>
     );
